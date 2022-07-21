fb4bba29c59755fa2a477b895071c443083462bd
Deva-Mari <devamari@gmx.net>
2022-07-21 13:38:24 +1200

    fixed teaser buttons and added cart button box shadow

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import styles from "../css/Button.module.css";
 import cart from "../assets/icon-cart.svg";
 const Button = () =>{
-    return(<button className={styles.button}><img src={cart}/> Add to cart</button>)
+    return(<button className={styles.button}><img src={cart} alt="cart icon"/> Add to cart</button>)
 }
 
 export default Button;
